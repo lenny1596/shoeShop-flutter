ded423c353686f0c14e194394bf6681bafccfca6
Lenin Bose <leninbose1596@gmail.com>
2024-07-04 00:08:10 +0530

    added the shop and cart page, and linked them to the bottom navbar using state.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ecommerce_flutter/components/bottom_navbar.dart';
 
+import 'cart_page.dart';
+import 'shop_page.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -9,11 +12,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // variable to control navigation, 0 = shop, 1 = cart
   int _selectedIndex = 0;
 
+// this method will help navigate between the pages
   void navigateBottomBar(int index) {
-    _selectedIndex = index;
+    setState(() {
+      _selectedIndex = index;
+    });
   }
+
+  // pages to switch
+  final List<Widget> _pages = [
+    // Shop page
+    const ShopPage(),
+    // Cart page
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
