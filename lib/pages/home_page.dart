0255cab8c27655c18eb39699a20d027bc4245741
Lenin Bose <leninbose1596@gmail.com>
2024-07-04 00:10:53 +0530

    Added an appBar menu that opens up a drawer that has a logo, home, about & logout button for now.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -37,6 +37,87 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: BottomNavbar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (context) => IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: const Padding(
+                padding: EdgeInsets.only(left: 12.0),
+                child: Icon(Icons.menu),
+              )),
+        ),
+      ),
+      drawer: Theme(
+        // Wrap the drawer in a theme inorder to set its default divider transparent.
+        data: ThemeData(
+            dividerTheme: const DividerThemeData(color: Colors.transparent)),
+        child: Drawer(
+          backgroundColor: Colors.grey[900],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  // logo
+                  DrawerHeader(
+                    child: Image.asset('lib/images/running-background.png'),
+                  ),
+
+                  // Home
+                  const Padding(
+                    padding: EdgeInsets.only(left: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.home,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        'Home',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+
+                  // About
+                  const Padding(
+                    padding: EdgeInsets.only(left: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.info,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        'About',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // Logout
+              const Padding(
+                padding: EdgeInsets.only(left: 25.0, bottom: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.logout,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'Logout',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
