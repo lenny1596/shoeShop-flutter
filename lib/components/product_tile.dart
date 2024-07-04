a2d5432666d502ccd163be93b960890cf87c34af
Lenin Bose <leninbose1596@gmail.com>
2024-07-05 00:33:21 +0530

    Added the custom product cards(tile) widget and linked it with shop page and model.

--- lib/components/product_tile.dart
+++ lib/components/product_tile.dart
@@ -10,22 +10,78 @@ class ProductTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(left: 25.0),
-      width: 260,
+      width: 250,
       decoration: BoxDecoration(
         color: Colors.grey[100],
         borderRadius: BorderRadius.circular(10.0),
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           // Image
           ClipRRect(
             borderRadius: BorderRadius.circular(10.0),
             child: Image.asset(product.imagePath),
           ),
-          // description
-          // price + details
 
-          // button to add-to-cart
+          // description
+          // Text(
+          //   product.description,
+          //   style: TextStyle(
+          //     color: Colors.grey[600],
+          //   ),
+          // ),
+
+          // price + details
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // product name
+                    Text(
+                      product.name,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+
+                    const SizedBox(
+                      height: 5,
+                    ),
+
+                    // price
+                    Text(
+                      '\$${product.price}',
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+
+                // add-to-cart button
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
